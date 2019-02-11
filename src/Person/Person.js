f4ef7d56d3642f1e768468e373a3d101f1f75eff
Zhihao <paradisetan798@gmail>
2019-02-11 13:49:51 +0200

    hook

---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const person = (props) =>{
+
+    return (
+        <div>
+            <p>im {props.name} and im {props.age}</p>
+            <p>{props.children}</p>
+        </div>
+    );
+    //return <p>im a person and im {Math.floor(Math.random()*30)} </p>
+};
+
+export default person;
