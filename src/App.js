6708bb6c9d1774e1aef451887671b0d8d6e46761
Zhihao <paradisetan798@gmail>
2019-02-12 14:29:30 +0200

    class base

--- src/App.js
+++ src/App.js
@@ -1,3 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+    state = {
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'Manu', age: 29 },
+            { name: 'Stephanie', age: 26 }
+        ],
+        otherState: 'some other value'
+    };
+
+    switchNameHandler = ( Name ) => {
+        // console.log('Was clicked!');
+        // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+        this.setState({
+            persons: [
+                { name: Name, age: 28 },
+                { name: 'Manu', age: 29 },
+                { name: 'Stephanie', age: 27 }
+            ]
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this,"Maximiliun")}
+                >
+                    My Hobbies: Racing
+                </Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                    click={this.switchNameHandler.bind(this,"Max!")}
+
+                />
+            </div>
+        );
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    }
+}
+
+export default App;
+
+
+/*
 import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
@@ -50,5 +109,5 @@ import Person from './Person/Person'
 
 export default app;
 
-
+*/
 
