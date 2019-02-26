840047e33b1fad5a36a05b8b860a8cacd6c6dd48
kesalomamatka <paradisetan798@gmail.com>
2019-02-26 20:02:32 +0200

    back

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,7 +7,7 @@ const person = (props) =>{
         <div className="Person">
             <p onClick={props.click}>im {props.name} and im {props.age}</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.change} value={props.name}/>
+            <input type="text" onChange={props.change<} value={props.name}/>
         </div>
     );
     //return <p>im a person and im {Math.floor(Math.random()*30)} </p>
