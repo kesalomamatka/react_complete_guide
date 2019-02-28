7f01fa377d195f62fa376b60684f4fa1690f364b
Zhihao <paradisetan798@gmail.com>
2019-02-28 15:41:50 +0200

    width

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Person.css';
 
 const person = (props) =>{
+
+
+
     return(
         <div className="Person">
             <p onClick={props.click}>Im {props.name} and im {props.age} years old</p>
