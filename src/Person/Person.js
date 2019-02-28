1038688018322a67edb99240163f8ec377380a06
Zhihao <paradisetan798@gmail.com>
2019-02-28 10:30:05 +0200

    css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
-const person = (Props) =>{
+const person = (props) =>{
     return(
-        <div>
-            <p>Im {Props.name} and im {Props.age} years old</p>
-            <p>{Props.children}</p>
+        <div className="Person">
+            <p onClick={props.click}>Im {props.name} and im {props.age} years old</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 };
