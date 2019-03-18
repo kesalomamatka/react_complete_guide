96f2625946fbd0b7c22e2861d0bf95e7eb8f7a25
kesalomamatka <paradisetan798@gmail.com>
2019-03-18 18:02:59 +0200

    69PseudoSelector

--- src/App.js
+++ src/App.js
@@ -71,16 +71,10 @@ class App extends Component {
 
     render () {
 
-        const style = {
-            backgroundColor: 'Green',
-            color: 'White',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        };
 
         let person = null;
+
+        let btnClass = "";
 
         if(this.state.showPersons){
             person = (
@@ -97,7 +91,7 @@ class App extends Component {
 
                 </div>
             );
-            style.backgroundColor="red";
+            btnClass = classes.Red;
 
         }
         const assignedClasses = [];
@@ -115,7 +109,7 @@ class App extends Component {
                     <h1>Hi, I'm a React App</h1>
                     <p className={assignedClasses.join(' ')}>This is really working!</p>
                     <button
-                        style={style}
+                        className={btnClass}
                         onClick={this.togglePersonsHandler}>Show Persons</button>
 
                         {person}
